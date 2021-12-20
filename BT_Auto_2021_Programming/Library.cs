31229c0ffab4bd029e34f85347fa343d78bd77f9
robert.chelarescu@yahoo.com <robert.chelarescu@yahoo.com>
2021-12-20 19:46:24 +0200

    Library

---
+++ BT_Auto_2021_Programming/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT_Auto_2021_Programming
+{
+    class Library
+    {
+        public static void Librarya(string[] args)
+        {
+            Book Book1 = new Book(" Medicina Spirituala", 33, "Alberto Villoldo", " Vidia ", 2015);
+            Book1.PrintBook();
+
+            Author Author1 = new Author("Alberto Villoldo", "albertovilloldo@gmail.com");
+            Author1.PrintAuthor();
+        }
+    }
+}
